0a9fe04d31ab61228d1f5e55148a4628920d54ed
Andreas Schneider <andreas-schneider89@gmx.de>
2022-02-23 17:24:36 +0100

    first commit

---
+++ src/main/java/com/schneider/andreas89/teamcontactsbackend/TeamContactsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.schneider.andreas89.teamcontactsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamContactsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamContactsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schneider/andreas89/teamcontactsbackend/TeamContactsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schneider.andreas89.teamcontactsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamContactsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
